a8f76be9ffad7ef9c0eb31bf7138638d2e657a1b
jc365 <35151037+jc365@users.noreply.github.com>
2018-03-14 02:33:40 +0100

    Add files via upload

---
+++ test_sscanf.ino
@@ -0,0 +1,14 @@
+int x;
+
+void setup() {
+  // put your setup code here, to run once:
+  x = 33;
+  pinMode(13, OUTPUT); 
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+
+  Serial.println("hola");
+  delay(5000);
+}
